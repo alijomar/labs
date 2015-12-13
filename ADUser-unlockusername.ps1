be599411d8d93d0c9624e77325f1a36ab727f72e
alijomar <amomar@outlook.com>
2015-12-12 20:00:55 -0500

    deleted ADUser-unlockusername

--- ADUser-unlockusername.ps1
+++
@@ -1,7 +0,0 @@
-# PowerShell script to unlock username in AD
-# Created by Ali Omar
-# Modified by
-# version 1.0
-
-Unlock-ADAccount -Identity (Read-Host "Enter username to unlock?") | FT Name,UserPrincipalName,employeeType,Description,telephoneNumber,LastLogonDate -AutoSize
-
